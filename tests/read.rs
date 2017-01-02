0c9448fbde045c9bad331e242b229e8f0a92e753
mitchmindtree <mitchell.nordine@gmail.com>
2017-01-03 00:01:22 +1100

    Clean up tests using new helper functions

--- tests/read.rs
+++ tests/read.rs
@@ -1,13 +1,5 @@
 extern crate m3u;
 
-fn path_entry(s: &str) -> m3u::Entry {
-    m3u::Entry::Path(s.into())
-}
-
-fn url_entry(s: &str) -> m3u::Entry {
-    m3u::Entry::Url(m3u::url::Url::parse(s).unwrap())
-}
-
 #[test]
 fn mixed() {
     let path = std::path::Path::new("tests/mixed.m3u");
@@ -15,44 +7,33 @@ fn mixed() {
     let mut entries = reader.entries();
 
     assert_eq!(entries.next().unwrap().unwrap(),
-               path_entry(r"Alternative\Band - Song.mp3"));
+               m3u::path_entry(r"Alternative\Band - Song.mp3"));
     assert_eq!(entries.next().unwrap().unwrap(),
-               path_entry(r"Classical\Other Band - New Song.mp3"));
+               m3u::path_entry(r"Classical\Other Band - New Song.mp3"));
     assert_eq!(entries.next().unwrap().unwrap(),
-               path_entry(r"Stuff.mp3"));
+               m3u::path_entry(r"Stuff.mp3"));
     assert_eq!(entries.next().unwrap().unwrap(),
-               path_entry(r"D:\More Music\Foo.mp3"));
+               m3u::path_entry(r"D:\More Music\Foo.mp3"));
     assert_eq!(entries.next().unwrap().unwrap(),
-               path_entry(r"..\Other Music\Bar.mp3"));
+               m3u::path_entry(r"..\Other Music\Bar.mp3"));
     assert_eq!(entries.next().unwrap().unwrap(),
-               url_entry(r"http://emp.cx:8000/Listen.pls"));
+               m3u::url_entry(r"http://emp.cx:8000/Listen.pls").unwrap());
     assert_eq!(entries.next().unwrap().unwrap(),
-               url_entry(r"http://www.example.com/~user/Mine.mp3"));
+               m3u::url_entry(r"http://www.example.com/~user/Mine.mp3").unwrap());
     assert!(entries.next().is_none());
 }
 
 #[test]
 fn ext() {
-
-    fn entry_ext(duration_secs: u64, name: String, entry: m3u::Entry) -> m3u::EntryExt {
-        m3u::EntryExt {
-            extinf: m3u::ExtInf {
-                duration_secs: duration_secs,
-                name: name,
-            },
-            entry: entry,
-        }
-    }
-
     let expected = vec![
-        entry_ext(123, "Sample artist - Sample title".into(),
-                  path_entry(r"C:\Documents and Settings\I\My Music\Sample.mp3")),
-        entry_ext(321, "Example Artist - Example title".into(),
-                  path_entry(r"C:\Documents and Settings\I\My Music\Greatest Hits\Example.ogg")),
-        entry_ext(123, "Sample artist - Sample title".into(),
-                  path_entry(r"Sample.mp3")),
-        entry_ext(321, "Example Artist - Example title".into(),
-                  path_entry(r"Greatest Hits\Example.ogg")),
+        m3u::path_entry(r"C:\Documents and Settings\I\My Music\Sample.mp3")
+            .extend(123, "Sample artist - Sample title"),
+        m3u::path_entry(r"C:\Documents and Settings\I\My Music\Greatest Hits\Example.ogg")
+            .extend(321, "Example Artist - Example title"),
+        m3u::path_entry(r"Sample.mp3")
+            .extend(123, "Sample artist - Sample title"),
+        m3u::path_entry(r"Greatest Hits\Example.ogg")
+            .extend(321, "Example Artist - Example title"),
     ];
 
     let path = std::path::Path::new("tests/ext.m3u");
--- tests/write.rs
+++ tests/write.rs
@@ -1,47 +1,40 @@
 extern crate m3u;
 
-fn path_entry(s: &str) -> m3u::Entry {
-    m3u::Entry::Path(s.into())
-}
-
-fn url_entry(s: &str) -> m3u::Entry {
-    m3u::Entry::Url(m3u::url::Url::parse(s).unwrap())
-}
-
 #[test]
 fn entry() {
 
-    let entries = vec![
-        path_entry(r"Alternative\Band - Song.mp3"),
-        path_entry(r"Classical\Other Band - New Song.mp3"),
-        path_entry(r"Stuff.mp3"),
-        path_entry(r"D:\More Music\Foo.mp3"),
-        path_entry(r"..\Other Music\Bar.mp3"),
-        url_entry(r"http://emp.cx:8000/Listen.pls"),
-        url_entry(r"http://www.example.com/~user/Mine.mp3"),
+    // Create a multimedia playlist.
+    let playlist = vec![
+        m3u::path_entry(r"Alternative\Band - Song.mp3"),
+        m3u::path_entry(r"Classical\Other Band - New Song.mp3"),
+        m3u::path_entry(r"Stuff.mp3"),
+        m3u::path_entry(r"D:\More Music\Foo.mp3"),
+        m3u::path_entry(r"..\Other Music\Bar.mp3"),
+        m3u::url_entry(r"http://emp.cx:8000/Listen.pls").unwrap(),
+        m3u::url_entry(r"http://www.example.com/~user/Mine.mp3").unwrap(),
     ];
 
-    const FILEPATH: &'static str = "tests/entries.m3u";
+    const FILEPATH: &'static str = "tests/playlist.m3u";
 
     if std::path::Path::new(FILEPATH).exists() {
         std::fs::remove_file(FILEPATH).unwrap();
     }
 
-    // Write the entries to the file.
+    // Write the playlist to the file.
     {
         let mut file = std::fs::File::create(FILEPATH).unwrap();
         let mut writer = m3u::Writer::new(&mut file);
-        for entry in &entries {
+        for entry in &playlist {
             writer.write_entry(entry).unwrap();
         }
         writer.flush().unwrap();
     }
 
-    // Read the entries from the file.
+    // Read the playlist from the file.
     {
         let mut reader = m3u::Reader::open(FILEPATH).unwrap();
-        let read_entries: Vec<_> = reader.entries().map(|entry| entry.unwrap()).collect();
-        assert_eq!(&entries, &read_entries);
+        let read_playlist: Vec<_> = reader.entries().map(|entry| entry.unwrap()).collect();
+        assert_eq!(&playlist, &read_playlist);
     }
 
     std::fs::remove_file(FILEPATH).unwrap();
@@ -50,48 +43,39 @@ fn entry() {
 #[test]
 fn entry_ext() {
 
-    fn entry_ext(duration_secs: u64, name: String, entry: m3u::Entry) -> m3u::EntryExt {
-        m3u::EntryExt {
-            extinf: m3u::ExtInf {
-                duration_secs: duration_secs,
-                name: name,
-            },
-            entry: entry,
-        }
-    }
-
-    let entries = vec![
-        entry_ext(123, "Sample artist - Sample title".into(),
-                  path_entry(r"C:\Documents and Settings\I\My Music\Sample.mp3")),
-        entry_ext(321, "Example Artist - Example title".into(),
-                  path_entry(r"C:\Documents and Settings\I\My Music\Greatest Hits\Example.ogg")),
-        entry_ext(123, "Sample artist - Sample title".into(),
-                  path_entry(r"Sample.mp3")),
-        entry_ext(321, "Example Artist - Example title".into(),
-                  path_entry(r"Greatest Hits\Example.ogg")),
+    // Create a multimedia playlist, including the duration in seconds and name for each entry.
+    let playlist = vec![
+        m3u::path_entry(r"C:\Documents and Settings\I\My Music\Sample.mp3")
+            .extend(123, "Sample artist - Sample title"),
+        m3u::path_entry(r"C:\Documents and Settings\I\My Music\Greatest Hits\Example.ogg")
+            .extend(321, "Example Artist - Example title"),
+        m3u::path_entry(r"Sample.mp3")
+            .extend(123, "Sample artist - Sample title"),
+        m3u::path_entry(r"Greatest Hits\Example.ogg")
+            .extend(321, "Example Artist - Example title"),
     ];
 
-    const FILEPATH: &'static str = "tests/entries_ext.m3u";
+    const FILEPATH: &'static str = "tests/playlist_ext.m3u";
 
     if std::path::Path::new(FILEPATH).exists() {
         std::fs::remove_file(FILEPATH).unwrap();
     }
 
-    // Write the entries to the file.
+    // Write the playlist to the file.
     {
         let mut file = std::fs::File::create(FILEPATH).unwrap();
         let mut writer = m3u::Writer::new_ext(&mut file).unwrap();
-        for entry in &entries {
+        for entry in &playlist {
             writer.write_entry(entry).unwrap();
         }
         writer.flush().unwrap();
     }
 
-    // Read the entries from the file.
+    // Read the playlist from the file.
     {
         let mut reader = m3u::Reader::open_ext(FILEPATH).unwrap();
-        let read_entries: Vec<_> = reader.entry_exts().map(|entry| entry.unwrap()).collect();
-        assert_eq!(&entries, &read_entries);
+        let read_playlist: Vec<_> = reader.entry_exts().map(|entry| entry.unwrap()).collect();
+        assert_eq!(&playlist, &read_playlist);
     }
 
     std::fs::remove_file(FILEPATH).unwrap();
